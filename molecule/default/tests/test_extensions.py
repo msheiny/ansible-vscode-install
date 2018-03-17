711481ad318b5dd4a693dc758b431ad712389148
Michael Sheinberg <m.sheiny@gmail.com>
2018-03-16 22:12:58 -0400

    Basic functionality working on Fedora + Debian

--- molecule/default/tests/test_extensions.py
+++ molecule/default/tests/test_extensions.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -8,3 +9,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_vscode_installed(host):
     assert host.package("code").is_installed
+
+
+@pytest.mark.parametrize("ext", ['gitlens', 'ms-python', 'vscodevim'])
+def test_vscode_extensions(host, ext):
+    EXT_DIR = "/home/reguser/.vscode/extensions"
+    ext_list = host.check_output("ls %s" % EXT_DIR)
+    assert ext in ext_list
